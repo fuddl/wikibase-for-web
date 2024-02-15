9a05ee430c9400bbfb4eab6b818c7f48ff3a8785
Alex <blank@MacBook-Pro.local>
2024-02-15 14:43:07 +0100

    more dates

--- templates/tempus/tempus.preprocess.mjs
+++ templates/tempus/tempus.preprocess.mjs
@@ -1,5 +1,7 @@
 export default ({ vars }) => {
 	vars.datetime = vars.time.replace(/^(\+|\-)/, '')
+	const negative = vars.time.startsWith('-')
+
 	if (vars.precision > 10) {
 		const date = new Date(vars.datetime)
 		vars.localised = date.toLocaleDateString()
@@ -38,7 +40,25 @@ export default ({ vars }) => {
 		const millenia = parseInt(vars.time.substring(1,2))
 		const milleniaOrdinal = millenia + 1
 		vars.localised = browser.i18n.getMessage("date_millenium", [milleniaOrdinal, millenia])
+	} else if (vars.precision < 6) {
+		const year = parseInt(vars.time.substring(1,6))
+		const deltas = {
+			'5': 10000,
+			'4': 100000,
+			'3': 1000000,
+			'2': 10000000,
+			'1': 100000000,
+		}
+		const delta = deltas[vars.precision]
+		const lowerBound = year - (delta / 2)
+		const upperBound = year + (delta / 2)
+		const deltaFormated = new Intl.NumberFormat().format(parseFloat((delta / 2)))
+		vars.localised = browser.i18n.getMessage("date_year_range", [year, lowerBound, upperBound, deltaFormated])
 	}
+	if (negative && vars.precision > 5) {
+		vars.localised = browser.i18n.getMessage("date_bce", [vars.localised])
+	}
+
 	vars.calendar = {
 		url: vars.calendarmodel
 	}
