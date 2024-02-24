d6dda148ee2adbf497890d9bbeb695866e251b3d
Alex <blank@MacBook-Pro.fritz.box>
2024-02-24 17:37:18 +0100

    keyboard controls

--- templates/choose/choose.events.mjs
+++ templates/choose/choose.events.mjs
@@ -20,47 +20,74 @@ export default ({ element, manager, state }) => [{
 			const itemTemplate = template.firstElementChild.cloneNode(true)
 
 			itemTemplate.setAttribute('href', item.url)
+			itemTemplate.dataset.id = item.title
 
 			const labelSlot = itemTemplate.querySelector('[name="label"]')
 			labelSlot.parentNode.replaceChild(document.createTextNode(item.label), labelSlot)
 
 			const descriptionSlot = itemTemplate.querySelector('[name="description"]')
 			descriptionSlot.parentNode.replaceChild(document.createTextNode(item.description), descriptionSlot)
-			
-			const idSlot = itemTemplate.querySelector('[name="id"]')
-			idSlot.parentNode.replaceChild(document.createTextNode(item.title), idSlot)
 
 			autocompleteWrapper.appendChild(itemTemplate)
 		})
+		e.target.classList.toggle('choose__type--picker-open', autocompleteWrapper.childElementCount > 0)
 	}
 }, {
 	id: 'keyboard-navigation',
 	target: element.querySelector('.choose__type'),
 	type: 'keydown',
 	listener: (e) => {
-		if (['ArrowDown', 'ArrowUp'].includes(e.key)) {
-			e.preventDefault()
-		}
-		const activeClass = 'choose__picker__pick--active';
-		const current = element.querySelector(`.${activeClass}`);
-		const picks = element.querySelectorAll('.choose__picker__pick');
-		const first = picks[0];
-		const last = picks[picks.length - 1];
-		let newCurrent = current;
+		if (['ArrowDown', 'ArrowUp', 'Enter'].includes(e.key)) {
+		    e.preventDefault();
+		    const scrollOptions = { behavior: 'smooth', block: 'nearest' }
 
-		switch (e.key) {
-			case 'ArrowDown':
-				newCurrent = current && current !== last ? current.nextElementSibling : first;
-				break;
-			case 'ArrowUp':
-				newCurrent = current && current !== first ? current.previousElementSibling : last;
-				break;
-		}
+		    const activeClass = 'choose__picker__pick--active';
+		    const current = element.querySelector(`.${activeClass}`);
+		    const picks = element.querySelectorAll('.choose__picker > .choose__picker__pick');
+		    const first = picks[0];
+		    const last = picks[picks.length - 1];
 
-		if (newCurrent && newCurrent !== current) {
-			current?.classList.remove(activeClass);
-			newCurrent.classList.add(activeClass);
-			newCurrent.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+		    if (current && e.key === 'Enter') {
+		    	e.target.nextElementSibling.innerText = current.dataset.id
+		    	e.target.value = current.querySelector('.choose__picker__pick-title').textContent.trim()
+		    	current.parentNode.innerText = ''
+		    }
+
+		    // If the user presses down while current is null, set first to be active and scroll into view
+		    if (!current && e.key === 'ArrowDown') {
+		        first.classList.add(activeClass);
+		        first.scrollIntoView(scrollOptions);
+		        return;
+		    }
+
+		    // Handling when the current is the last and the user presses down, do nothing
+		    if (current === last && e.key === 'ArrowDown') {
+		        return;
+		    }
+
+		    // Handling ArrowDown: Set the next element sibling active
+		    if (e.key === 'ArrowDown' && current !== last) {
+		        const next = current.nextElementSibling;
+		        current.classList.remove(activeClass);
+		        next.classList.add(activeClass);
+		        next.scrollIntoView(scrollOptions);
+		        return;
+		    }
+
+		    // Handling ArrowUp: If the first is active, element should scroll into view
+		    if (e.key === 'ArrowUp' && current === first) {
+		    	current.classList.remove(activeClass)
+		        e.target.scrollIntoView();
+		        return;
+		    }
+
+		    // Handling ArrowUp: Set the previous element sibling active
+		    if (e.key === 'ArrowUp' && current !== first) {
+		        const previous = current.previousElementSibling;
+		        current.classList.remove(activeClass);
+		        previous.classList.add(activeClass);
+		        previous.scrollIntoView(scrollOptions);
+		    }
 		}
 	},
 }]
