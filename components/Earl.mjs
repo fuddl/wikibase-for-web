a5f79f4687547eb0d7a3a8e35f8e480c2e8c8d31
Alex <blank@MacBook-Pro.fritz.box>
2024-03-08 17:00:23 +0100

    misc

--- components/Earl.mjs
+++ components/Earl.mjs
@@ -1,9 +1,14 @@
 import { h, Component } from '../node_modules/preact/dist/preact.mjs';
 import htm from '../node_modules/htm/dist/htm.mjs';
+import { requireStylesheet } from '../modules/requireStylesheet.mjs';
 
 const html = htm.bind(h);
 
 class Earl extends Component {
+  componentDidMount() {
+    requireStylesheet(browser.runtime.getURL('/components/earl.css'));
+  }
+
   render({ value: href }) {
     const shortUrl = href
       .replace(/^[a-z]+\:\/\//, '')
@@ -11,7 +16,7 @@ class Earl extends Component {
       .replace(/\/index\.(php|html?)$/, '')
       .replace(/\/$/, '');
 
-    return html`<a href="${href}">${shortUrl}</a>`;
+    return html`<a class="earl" href="${href}">${shortUrl}</a>`;
   }
 }
 
