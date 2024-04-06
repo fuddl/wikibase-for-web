d89485a44d5273a27272fbc465b932514c0e5126
Alex <blank@MacBook-Pro-3.fritz.box>
2024-04-06 10:13:15 +0200

    don't offer edits for wikis without write permission

--- sidebar/organise-view.js
+++ sidebar/organise-view.js
@@ -1,4 +1,37 @@
-function organiseView({ candidates }) {
+import wikibases from '../wikibases.mjs';
+
+async function filterEditPermissions(unmatched) {
+	return (
+		await Promise.all(
+			unmatched.map(
+				item =>
+					new Promise(async (resolve, reject) => {
+						const endpoint = wikibases[item.instance].api.instance.apiEndpoint;
+
+						try {
+							const response = await fetch(
+								`${endpoint}?action=query&meta=userinfo&uiprop=rights&format=json`,
+							);
+							if (!response.ok) {
+								resolve(null);
+							}
+
+							const data = await response.json();
+							const rights = data.query.userinfo.rights;
+
+							const hasPermissions =
+								rights.includes('writeapi') && rights.includes('edit');
+							resolve(hasPermissions ? item : null);
+						} catch (error) {
+							reject(error);
+						}
+					}),
+			),
+		)
+	).filter(item => item !== null);
+}
+
+async function organiseView({ candidates }) {
 	const directMatches = candidates
 		.filter(item => item?.resolved.length > 0)
 		.map(item => item.resolved)
@@ -12,20 +45,24 @@ function organiseView({ candidates }) {
 		parseInt(item.specificity),
 	);
 
-	const propertySpecificities = candidates
-		.filter(item => item.resolved.length === 0)
-		.map(item => parseInt(item.specificity));
+	const unmatched = candidates.filter(item => item.resolved.length === 0);
+
+	const matchable = await filterEditPermissions(unmatched);
+
+	const propertySpecificities = matchable.map(item =>
+		parseInt(item.specificity),
+	);
 
 	const entityMaxSpecific = Math.max(...entitySpecificities);
 	const propertyMaxSecific = Math.max(...propertySpecificities);
 
-	const propertiesHigherSpecificity = candidates
-		.filter(item => item.resolved.length === 0)
-		.filter(item => item.specificity > entityMaxSpecific);
+	const propertiesHigherSpecificity = matchable.filter(
+		item => item.specificity > entityMaxSpecific,
+	);
 
-	const propertiesLowerSpecificity = candidates
-		.filter(item => item.resolved.length === 0)
-		.filter(item => item.specificity < entityMaxSpecific);
+	const propertiesLowerSpecificity = matchable.filter(
+		item => item.specificity < entityMaxSpecific,
+	);
 
 	const bestMatches = directMatches
 		// only the most specific item
