d16cb1cdb7336aa60b02c8e65fa350db2b0dbc2c
Alex <blank@MacBook-Pro-3.fritz.box>
2024-05-11 15:40:17 +0200

    only get short names of the active language

--- queries/shortTitle.mjs
+++ queries/shortTitle.mjs
@@ -5,7 +5,7 @@ export const shortTitle = {
 		SELECT ?short WHERE {
 			wd:${params.subject} wdt:${instance.props.shortTitle} ?short.
 			BIND( LANG(?short) AS ?language).
-			FILTER (?language in ('${instance.languages.join("', '")}'))
+			FILTER (?language  = '${instance.languages[0].replace(/\-.+/, '')}')
 		} order by desc(strlen(?short))
 	`,
 	cacheTag: ({ instance, params }) =>
